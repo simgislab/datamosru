13372f08c9e21875b186976d3720ce4716d925b2
simgislab <sim@gis-lab.info>
2013-05-31 11:38:17 +0400

    output to console

--- cleanup.py
+++ cleanup.py
@@ -16,8 +16,11 @@ dirs = os.walk('.').next()[1]
 for adir in dirs:
     if adir != "_listings":
         os.chdir(adir)
-        if os.path.exists(adir + "_temp.csv"): os.remove(adir + "_temp.csv")
+        if os.path.exists(adir + "_temp.csv"): 
+        	print("Removed: " + adir + "_temp.csv")
+        	os.remove(adir + "_temp.csv")
         if os.path.exists(adir + ".csv") and not os.path.exists(adir + ".zip"):
+        	print("Zipped: " + adir + ".csv")
             fz = zipfile.ZipFile(adir + ".zip",'w')
             fz.write(adir + ".csv", compress_type=zipfile.ZIP_DEFLATED)
             fz.close()
