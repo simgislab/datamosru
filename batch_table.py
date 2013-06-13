087dc7322bfb0e9ba2eb8768d92fe6ae813a13e2
Maxim Dubinin <sim@gis-lab.info>
2013-06-13 22:53:28 +0400

    compare to previous data and get change, show in colors

--- batch_table.py
+++ batch_table.py
@@ -8,16 +8,28 @@ import urllib2
 f = "_general.csv"
 f = urllib2.urlopen("http://gis-lab.info/data/mos.ru/data/_listings/_general.csv")
 reader = csv.reader(f, delimiter=";" )
+date = "20130611"
 
-print """==Таблица данных==
+fon = "table.wiki"
+fo = open(fon,"w")
+
+fo.write("""===Скачать данные===
+
+Поле изменение показывает сравнение с версией от %s.
+
+Цветами показаны: <span style="background-color:LightGreen">увеличение</span> или <span style="background-color:Salmon">уменьшение</span> количества объектов, <span style="background-color:Yellow">новый набор</span> (отсутствовавший %s)
+""" % (date,date))
+
+fo.write("""
 {| class="wikitable sortable" border="1" width="100%"
 ! | Код
 ! | Описание набора
 ! | Категория
 ! | Объектов
+! | Изменение
 ! | Геоданные
 ! | Оригинал
-! | Данные\n"""
+! | Данные\n""")
 
 first = True
 for row in reader:
@@ -37,17 +49,60 @@ for row in reader:
         datalink_osm = ""
         
     #count number of objects
-    f = open("data-norm/norm-20130611/"+code+".csv","rb")
-    numobj = len(f.readlines()) - 1
-    f.close()
-    
-    print """|-
-| %s
-| %s
-| %s
-| %s
-| %s
-| %s
-| %s<br>%s<br>%s""" % (code,descr,cat.decode('utf-8').encode('cp1251'),numobj,geo.decode('utf-8').encode('cp1251'),datalink,datalink_norm,datalink_shp,datalink_osm)
+    print("data-norm/norm-" + date + "/"+code+".csv")
+    if os.path.isfile("data-norm/norm-20130611/"+code+".csv") == False:
+        print("This file is missing in local folder and skipped in the table")
+        numobj = "X"
+        style = "bgcolor=\"Yellow\" | "
+        change = "еще не импортирован"
+        datalink_norm = "еще не импортирован"
+        datalink_shp = ""
+        datalink_osm = ""
+    else:
+        f = open("data-norm/norm-20130611/"+code+".csv","rb")
+        numobj = len(f.readlines()) - 1
+        f.close()
+        
+        #change compared to prev version
+        if os.path.isfile("data-prev/"+code+".csv"):
+            datafileprev = open("data-prev/"+code+".csv","rb")
+            numobjprev = len(datafileprev.readlines()) - 1
+            datafileprev.close()
+            change = numobj - numobjprev
+        else:
+            change = ""
 
-print """|}"""
+        
+        
+        if change == "":
+            style = "bgcolor=\"Yellow\" | "
+        elif change > 0:
+            style = "bgcolor=\"LightGreen\" | "
+            change = "+" + str(change)
+        elif change < 0:
+            style = "bgcolor=\"Salmon\" | "
+        else:
+            style = ""
+        
+    fo.write("""|-
+| %s
+| %s
+| %s
+| %s
+| %s%s
+| %s
+| %s
+| %s<br>%s<br>%s
+""" % (code,
+                   descr,
+                   cat.decode('utf-8').encode('cp1251'),
+                   numobj,
+                   style, change,
+                   geo.decode('utf-8').encode('cp1251'),
+                   datalink,
+                   datalink_norm,
+                   datalink_shp,
+                   datalink_osm))
+
+fo.write("""|}""")
+fo.close()
